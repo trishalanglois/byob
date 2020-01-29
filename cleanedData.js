39b45259c750b891c322c7ce28e4ce6645da8e4f
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-29 16:32:57 -0700

    Add all death data to table

--- cleanedData.js
+++ cleanedData.js
@@ -1,24 +1,13 @@
 const countriesData = require('./countriesData');
 const deathsData = require('./deathsData');
 
+const cleanedData = countriesData.map(country => { 
+  let myDeaths = deathsData.filter(death => death.country === country.name);
+  return {
+    name: country.name,
+    alpha: country.alpha,
+    deaths: myDeaths
+  }
+})
 
-//input countries array  -- map
-// create new object for each country
-// create new key with deaths in that country 
-// if no key exists yet, make "death" key, then push death object into it, maybe cleaning it here, too?
-//output countries array of same length
-
-const cleanedData = () => {
-  return countriesData.map(country => {
-    return {
-      name: country.name,
-      alpha: country.alpha,
-      deaths: []
-    }
-  })
-} 
-
-
-//separate function to push deaths into object?
-
-module.exports = cleanedData();
+module.exports = cleanedData;
--- db/seeds/dev/countries.js
+++ db/seeds/dev/countries.js
@@ -1,17 +1,32 @@
 const cleanedData = require('../../../cleanedData');
 
+// console.log('3', cleanedData[0]);
+
 const createCountry = async (knex, country) => {
   const countryId = await knex('countries').insert({
     country_abbrev: country.alpha,
     country_name: country.name
   }, 'id');
-  
+
+  let deathPromises = country.deaths.map(death => {
+    return createDeath(knex, {
+      date: death.date,
+      cause_of_death: death.cause,
+      country_id: countryId[0]
+    })
+  })
+  return Promise.all(deathPromises)
 };
+
+const createDeath = (knex, death) => {
+  return knex('deaths').insert(death);
+}
 
 exports.seed = async (knex) => {
   try {
     await knex('deaths').del()
     await knex('countries').del()
+
     let countriesPromises = cleanedData.map(country => {
       return createCountry(knex, country)
     })
