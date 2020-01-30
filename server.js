ffd8aeb2f49c24bf4375edbb5b4b2068022ffc62
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-30 11:51:39 -0700

    Add GET endpoint for getting deaths of a specific country

--- server.js
+++ server.js
@@ -26,11 +26,23 @@ app.get('/api/v1/countries', async (request, response) =>  {
 app.get('/api/v1/countries/:id', async (request, response) => {
   const { id } = request.params;
   const countries = await database('countries').select();
-  console.log(app.locals);
   const country = countries.find(country => country.id === parseInt(id));
 
   country ? response.status(200).json(country) : response.status(404).json({ error: `Unable to find a country with the id of ${id}.  Please try again.`})
 })
+
+//get deaths for a given country
+app.get('/api/v1/countries/:id/deaths', async (request, response) => {
+  const { id } = request.params;
+  const countries = await database('countries').select();
+  const country = countries.find(country => country.id === parseInt(id));
+  const deaths = await database('deaths').select();
+  const countryDeaths = deaths.filter(death => death.country_id === parseInt(id))
+
+  country && countryDeaths ? response.status(200).json(countryDeaths) : response.status(404).json({ error: `Unable to find deaths that match country id of ${id}.  Please try another country id.`})
+})
+
+//get specific death for a given country
 
 
 
